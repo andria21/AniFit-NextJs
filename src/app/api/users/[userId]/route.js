c48760a1ae635e0fd682363c571e7b88d5ddeb4b
andria21 <andriamarqarovi33@gmail.com>
2024-02-19 19:43:23 +0000

    Added delete users functionality. changed exercise card ui

--- src/app/api/users/[userId]/route.js
+++ src/app/api/users/[userId]/route.js
@@ -1,16 +1,14 @@
 import { NextResponse } from "next/server";
 import connect from "@/utils/db";
 import User from "@/models/User";
-export const dynamic = 'force-dynamic'
 
-export const GET = async (request) => {
+export const DELETE = async (request, { params }) => {
+  const { userId } = params;
   try {
     await connect();
+    await User.findByIdAndDelete(userId);
 
-    const users = await User.find();
-
-
-    return new NextResponse(JSON.stringify(users), { status: 200 });
+    return new NextResponse("Exercise user has been deleted", { status: 200 });
   } catch (err) {
     return new NextResponse("Database Error", { status: 500 });
   }
--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,38 +1,100 @@
 "use client";
+
+import { useSession } from "next-auth/react";
 import styles from "./page.module.css";
 
 import useSWR from "swr";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 // import dynamic from "next/dynamic";
 
 export default function Users() {
   const adminEmail = process.env.ADMIN_EMAIL;
+  const session = useSession();
+  const router = useRouter();
+
+  const [confirmDelete, setConfirmDelete] = useState(false);
+  const [userIdToDelete, setUserIdToDelete] = useState(null);
 
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
   const { data, mutate, error, isLoading } = useSWR(`/api/users`, fetcher);
 
-  // !isLoading && console.log(data, error);
+  if (session.status === "unauthenticated") {
+    router?.push("/dashboard/login");
+  }
 
-  return (
-    <div className={styles.container}>
-      <h2>All available users:</h2>
-      <div className={styles.usersContainer}>
-        {isLoading
-          ? "Loading..."
-          : data.map((user) => (
-              <div key={user._id}>
-                <div className={styles.userContainer}>
-                  <div className={styles.userUsername}>
-                    <h4>{user.name}</h4>
-                    <h4>{user.email}</h4>
+  const handleDeleteUser = async (userId) => {
+    setUserIdToDelete(userId);
+    setConfirmDelete(true);
+  };
+
+  const confirmDeleteAction = async () => {
+    try {
+      await fetch(`/api/users/${userIdToDelete}`, {
+        method: "DELETE",
+      });
+      mutate();
+      setConfirmDelete(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const cancelDeleteAction = () => {
+    setConfirmDelete(false);
+    setUserIdToDelete(null);
+  };
+
+  // !isLoading && console.log(data, error);
+  if (
+    session.status === "authenticated" &&
+    session.data.user.email === adminEmail
+  ) {
+    return (
+      <div className={styles.container}>
+        <h2>All available users:</h2>
+        <div className={styles.usersContainer}>
+          {isLoading
+            ? "Loading..."
+            : data.map((user) => (
+                <div key={user._id}>
+                  <div className={styles.userContainer}>
+                    <div className={styles.userUsername}>
+                      <h4>{user.name}</h4>
+                      <h4>{user.email}</h4>
+                      <button
+                        className={styles.delete}
+                        onClick={() => handleDeleteUser(user._id)}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                    <br />
                   </div>
-                  <br />
                 </div>
-              </div>
-            ))}
+              ))}
+        </div>
+        {confirmDelete && (
+          <div className={styles.answersContainer}>
+            <p>Are you sure you want to delete this user?</p>
+            <button
+              onClick={confirmDeleteAction}
+              className={styles.answerButton}
+            >
+              Yes
+            </button>
+            <button
+              onClick={cancelDeleteAction}
+              className={styles.answerButton}
+            >
+              No
+            </button>
+          </div>
+        )}
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 // export default dynamic(() => Promise.resolve(Users), { ssr: false });
