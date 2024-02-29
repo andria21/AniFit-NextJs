16f01c4d04193ff66002e13a8b5e4079660567d4
andria21 <andriamarqarovi33@gmail.com>
2024-02-29 17:58:56 +0000

    testing nodemailer with pwa

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -5,7 +5,8 @@ import bcrypt from "bcryptjs";
 import nodemailer from "nodemailer";
 
 export const POST = async (request) => {
-  const { name, email, password, sharedItems, age, weight, height, gender } = await request.json();
+  const { name, email, password, sharedItems, age, weight, height, gender } =
+    await request.json();
 
   await connect();
 
@@ -23,7 +24,6 @@ export const POST = async (request) => {
   });
 
   try {
-
     // const transporter = nodemailer.createTransport({
     //   service: 'gmail',
     //   host: 'smpt.gmail.com',
@@ -49,6 +49,9 @@ export const POST = async (request) => {
     // await transporter.sendMail(mailOption)
 
     await newUser.save();
+
+    await sendEmailNotification(name, email);
+
     return new NextResponse("User has been created!", {
       status: 201,
     });
@@ -58,3 +61,35 @@ export const POST = async (request) => {
     });
   }
 };
+
+async function sendEmailNotification(name, email) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smpt.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.NEXT_EMAIL,
+      pass: process.env.NEXT_EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: "andriamarqarovi21@gmail.com",
+    to: "annikni@gmail.com",
+    subject: "New user just registered on the website!",
+    html: `
+      <h3>Hello Ani, a new user has been registered.</h3>
+      <p>Name: ${name}</p>
+      <p>Email: ${email}</p>
+      `,
+  };
+
+  try {
+    // Send email
+    await transporter.sendMail(mailOptions);
+    console.log("Email notification sent successfully");
+  } catch (error) {
+    console.error("Error sending email notification:", error);
+  }
+}
