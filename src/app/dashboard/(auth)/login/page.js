6da70a26c16e4d82f95113c8ce6154c73f1dcb8d
andria21 <andriamarqarovi33@gmail.com>
2024-03-02 22:10:44 +0000

    styled login page

--- src/app/dashboard/(auth)/login/page.js
+++ src/app/dashboard/(auth)/login/page.js
@@ -15,7 +15,11 @@ export default function Login() {
   const adminEmail = process.env.ADMIN_EMAIL;
 
   if (session.status === "loading") {
-    return <div className={styles.container}><Spinner /></div>;
+    return (
+      <div className={styles.container}>
+        <Spinner />
+      </div>
+    );
   }
 
   if (
@@ -42,47 +46,47 @@ export default function Login() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.mainContainer}>
-        <form className={styles.loginForm} onSubmit={handleLogin}>
-          <h2 className={styles.title}>Login</h2>
-          <div className={styles.formGroup}>
-            <label htmlFor="email" className={styles.label}>
-              Email:
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className={styles.input}
-              required
-            />
-          </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="password" className={styles.label}>
-              Password:
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className={styles.input}
-              required
-            />
-          </div>
-          <button type="submit" className={styles.loginButton}>
+      <input id={styles.signupToggle} type="checkbox" />
+      <form className={styles.form} onSubmit={handleLogin}>
+        <div className={styles.formFront}>
+          <div className={styles.formDetails}>Login</div>
+          <input
+            type="text"
+            name="email"
+            className={styles.input}
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            name="password"
+            className={styles.input}
+            placeholder="Password"
+          />
+          <button className={styles.btn} type="submit">
             Login
           </button>
-          <div className={styles.newDiv}>
-            <button
-              type="text"
-              onClick={() => router?.push("/dashboard/register")}
-              className={styles.newAccButton}
-            >
-              - Create a new account -
-            </button>
-          </div>
-        </form>
-      </div>
+          <span className={styles.switch}>
+            Don't have an account?
+            <label htmlFor="signupToggle" className={styles.signupTog} onClick={() => router?.push("/dashboard/register")}>
+              Sign Up
+            </label>
+          </span>
+        </div>
+      </form>
     </div>
   );
 }
+
+// <div className={styles.formBack}>
+//               <div className={styles.formDetails}>Sign Up</div>
+//               <input type="text" className={styles.input} placeholder="Firstname" />
+//               <input type="text" className={styles.input} placeholder="Username" />
+//               <input type="password" className={styles.input} placeholder="Password" />
+//               <input type="password" className={styles.input} placeholder="Confirm Password" />
+//               <button className={styles.btn}>Sign Up</button>
+//               <span className={styles.switch}>Already have an account?
+//                   <label htmlFor="signup_toggle" className={styles.signupTog}>
+//                       Sign In
+//                   </label>
+//               </span>
+//           </div>
