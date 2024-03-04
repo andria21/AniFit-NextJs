58c44d6548d8d7715cd44bfc51321da8a938c1d1
andria21 <andriamarqarovi33@gmail.com>
2024-03-04 03:44:33 +0000

    small changes to logo and some other components

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,7 +33,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       <Head>
         <link rel="shortcut icon" href="./favicon.ico" />
       </Head>
